275b61515a680c7f6ed7921035410acfedc3bb19
Júlio Melo <juliovilar93@gmail.com>
2018-10-23 10:38:19 -0300

    adicionados comandos do git

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello word");
+		System.out.println("Clonar repositório");
+		System.out.println("git clone");
+		System.out.println("==========");
+		System.out.println("Criar branch e alterar para a mesma");
+		System.out.println("git branch -b nomeDaBranch");
+		System.out.println("==========");
+		System.out.println("Adicionar mudanças feitas");
+		System.out.println("git add .");
+		System.out.println("==========");
+		System.out.println("Commitar mudanças realizadas");
+		System.out.println("git commit -m mensagemDoCommit");
+		System.out.println("==========");
+		System.out.println("Enviar para o repositorio remoto");
+		System.out.println("git push origin master");
+		System.out.println("==========");
 	}
 
 }
